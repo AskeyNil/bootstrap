377247c56ccd9ba19e9d0f708606298aee511f0d
AskeyNil <askeynil@gmail.com>
2026-06-23 00:04:53 +0000

    refactor(lib/brew.sh): 提取 _activate_brew 消除路径检测重复
    - 将重复的 /opt/homebrew / /usr/local/bin/brew 路径检测提取为 _activate_brew
    - Homebrew 安装 curl 添加 --proto '=https'
    - dry-run 信息同步更新

--- lib/brew.sh
+++ lib/brew.sh
@@ -1,34 +1,29 @@
 #!/usr/bin/env bash
 
-ensure_brew() {
-    if have brew; then
-        return
-    fi
-
+_activate_brew() {
     if [[ -x /opt/homebrew/bin/brew ]]; then
         eval "$(/opt/homebrew/bin/brew shellenv)"
-        return
+        return 0
     fi
-
     if [[ -x /usr/local/bin/brew ]]; then
         eval "$(/usr/local/bin/brew shellenv)"
-        return
+        return 0
     fi
+    return 1
+}
+
+ensure_brew() {
+    have brew && return 0
+    _activate_brew && return 0
 
     log "installing Homebrew"
     if [[ "${BOOTSTRAP_DRY_RUN}" == "1" ]]; then
-        log "dry-run: NONINTERACTIVE=1 /bin/bash -c \"\$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)\""
+        log "dry-run: NONINTERACTIVE=1 /bin/bash -c \"\$(curl -fsSL --proto '=https' https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)\""
         return
     fi
 
-    NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-
-    if [[ -x /opt/homebrew/bin/brew ]]; then
-        eval "$(/opt/homebrew/bin/brew shellenv)"
-    elif [[ -x /usr/local/bin/brew ]]; then
-        eval "$(/usr/local/bin/brew shellenv)"
-    fi
-
+    NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL --proto '=https' https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    _activate_brew
     require_command brew
 }
 
